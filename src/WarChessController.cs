5d7a11edbe814dd3b0172d73c2f530ceaf8d19ec
Jenocn <544673717@qq.com>
2023-06-25 20:13:03 +0800

    布局、交互单元格

--- src/WarChessController.cs
+++ src/WarChessController.cs
@@ -10,14 +10,45 @@ namespace WarChess {
 		/// 获得精灵的可行动单元格
 		/// </summary>
 		public static WarChessNode<WarChessCell> CreateActionCellTree(WarChessSpace space, WarChessSprite sprite) {
-			WarChessNode<WarChessCell> ret = null;
+			WarChessNode<WarChessCell> root = null;
 			var cell = space.GetCell(sprite.x, sprite.y);
 			if (cell != null) {
 				// tree
-				ret = new WarChessNode<WarChessCell>(cell);
-				_SearchActionSideCells(ret, space, sprite);
+				root = new WarChessNode<WarChessCell>(cell);
+
+				var pool = new Queue<WarChessNode<WarChessCell>>();
+				var close = new Dictionary<WarChessCell, WarChessNode<WarChessCell>>();
+
+				pool.Enqueue(root);
+				close.Add(root.value, root);
+				var tempArr = new WarChessCell[4];
+				while (pool.Count > 0) {
+					var node = pool.Dequeue();
+
+					tempArr[0] = node.value.top;
+					tempArr[1] = node.value.bottom;
+					tempArr[2] = node.value.left;
+					tempArr[3] = node.value.right;
+
+					for (int i = 0; i < tempArr.Length; ++i) {
+						var value = tempArr[i];
+						if (_CheckActionCell(value, space, node, sprite, out var sumCostAP)) {
+							if (close.TryGetValue(value, out var f)) {
+								if (sumCostAP >= f.customData) {
+									continue;
+								} else {
+									close.Remove(value);
+								}
+							}
+							var newNode = node.AddChild(tempArr[i]);
+							newNode.customData = sumCostAP;
+							pool.Enqueue(newNode);
+							close.Add(value, newNode);
+						}
+					}
+				}
 			}
-			return ret;
+			return root;
 		}
 
 		/// <summary>
@@ -34,7 +65,9 @@ namespace WarChess {
 
 			while (pool.Count > 0) {
 				var tempNode = pool.Dequeue();
-				ret.AddLast(tempNode.value);
+				if (!ret.Contains(tempNode.value)) {
+					ret.AddLast(tempNode.value);
+				}
 				foreach (var item in tempNode.children) {
 					pool.Enqueue(item);
 				}
@@ -63,6 +96,33 @@ namespace WarChess {
 			return GeneratorWay(root, cell.x, cell.y);
 		}
 
+		/// <summary>
+		/// 生成可交互的单元格
+		/// </summary>
+		/// <param name="actionCells">行动范围内的单元格</param>
+		/// <param name="sprite">当前行动的精灵</param>
+		/// <param name="layout">交互范围的布局</param>
+		/// <param name="includeActionCells">是否包含行动单元格本身</param>
+		public static HashSet<WarChessCell> GeneratorInteractionCells(LinkedList<WarChessCell> actionCells, WarChessSpace space, WarChessSprite sprite, WarChessLayout layout, bool includeActionCells) {
+			var ret = new HashSet<WarChessCell>();
+			foreach (var item in actionCells) {
+				var tempSpr = space.GetSprite(item.x, item.y);
+				if (tempSpr != null && tempSpr != sprite) { continue; }
+				foreach (var point in layout.points) {
+					var x = item.x + point.Item1;
+					var y = item.y + point.Item2;
+					var tempCell = space.GetCell(x, y);
+					if (tempCell != null) {
+						if (!includeActionCells && actionCells.Contains(tempCell)) {
+							continue;
+						}
+						ret.Add(tempCell);
+					}
+				}
+			}
+			return ret;
+		}
+
 		private static WarChessNode<WarChessCell> _FindMinAPNode(WarChessNode<WarChessCell> root, int x, int y) {
 			WarChessNode<WarChessCell> ret = null;
 			foreach (var item in root.children) {
@@ -81,40 +141,6 @@ namespace WarChess {
 				}
 			}
 			return ret;
-		}
-
-		private static void _SearchActionSideCells(WarChessNode<WarChessCell> root, WarChessSpace space, WarChessSprite sprite) {
-			var pool = new Queue<WarChessNode<WarChessCell>>();
-			var close = new Dictionary<WarChessCell, WarChessNode<WarChessCell>>();
-
-			pool.Enqueue(root);
-			close.Add(root.value, root);
-			var tempArr = new WarChessCell[4];
-			while (pool.Count > 0) {
-				var node = pool.Dequeue();
-
-				tempArr[0] = node.value.top;
-				tempArr[1] = node.value.bottom;
-				tempArr[2] = node.value.left;
-				tempArr[3] = node.value.right;
-
-				for (int i = 0; i < tempArr.Length; ++i) {
-					var value = tempArr[i];
-					if (_CheckActionCell(value, space, node, sprite, out var sumCostAP)) {
-						if (close.TryGetValue(value, out var f)) {
-							if (sumCostAP >= f.customData) {
-								continue;
-							} else {
-								close.Remove(value);
-							}
-						}
-						var newNode = node.AddChild(tempArr[i]);
-						newNode.customData = sumCostAP;
-						pool.Enqueue(newNode);
-						close.Add(value, newNode);
-					}
-				}
-			}
 		}
 
 		private static bool _CheckActionCell(WarChessCell cell, WarChessSpace space, WarChessNode<WarChessCell> parent, WarChessSprite sprite, out float sumCostAP) {
