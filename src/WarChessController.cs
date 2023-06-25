3ca56065f8a8b48f9b0c92a581dc3ea6f973079f
Jenocn <544673717@qq.com>
2023-06-25 18:41:38 +0800

    优化获取可通行单元格的方法，使用广度优先方式

--- src/WarChessController.cs
+++ src/WarChessController.cs
@@ -15,47 +15,38 @@ namespace WarChess {
 			if (cell != null) {
 				// tree
 				ret = new WarChessNode<WarChessCell>(cell);
-				_SearchActionSideCells(ret, space, sprite, sprite.GetBaseActionPoint(), 0);
+				_SearchActionSideCells(ret, space, sprite);
 			}
 			return ret;
 		}
 
 		/// <summary>
-		/// 根据树形结构的数据生成列表数据
+		/// 生成列表数据
 		/// </summary>
 		public static LinkedList<WarChessCell> GeneratorCellList(WarChessNode<WarChessCell> node) {
 			var ret = new LinkedList<WarChessCell>();
-			if (node.value != null) {
-				ret.AddLast(node.value);
-				_GeneratorCellListFromChildren(node, in ret);
+			if (node.value == null) {
+				return ret;
 			}
+
+			var pool = new Queue<WarChessNode<WarChessCell>>();
+			pool.Enqueue(node);
+
+			while (pool.Count > 0) {
+				var tempNode = pool.Dequeue();
+				ret.AddLast(tempNode.value);
+				foreach (var item in tempNode.children) {
+					pool.Enqueue(item);
+				}
+			}
+
 			return ret;
-		}
-
-		/// <summary>
-		/// 在所有子节点中获得从根节点到目标坐标的最短层级(格子最少)的节点集合
-		/// </summary>
-		public static LinkedList<WarChessCell> GeneratorWay_MinimumLayer(WarChessNode<WarChessCell> root, int x, int y) {
-			var tempList = new LinkedList<WarChessCell>();
-			var node = _FindMinLayerNode(root, x, y);
-			while (node != null) {
-				tempList.AddFirst(node.value);
-				node = node.parent;
-			}
-			return tempList;
-		}
-
-		/// <summary>
-		/// 在所有子节点中获得从根节点到目标节点的最短层级(格子最少)的节点集合
-		/// </summary>
-		public static LinkedList<WarChessCell> GeneratorWay_MinimumLayer(WarChessNode<WarChessCell> root, WarChessCell cell) {
-			return GeneratorWay_MinimumLayer(root, cell.x, cell.y);
 		}
 
 		/// <summary>
 		/// 在所有子节点中获得从根节点到目标坐标的最小行动力(AP最少)的节点集合
 		/// </summary>
-		public static LinkedList<WarChessCell> GeneratorWay_MinimumAP(WarChessNode<WarChessCell> root, int x, int y) {
+		public static LinkedList<WarChessCell> GeneratorWay(WarChessNode<WarChessCell> root, int x, int y) {
 			var tempList = new LinkedList<WarChessCell>();
 			var node = _FindMinAPNode(root, x, y);
 			while (node != null) {
@@ -68,29 +59,8 @@ namespace WarChess {
 		/// <summary>
 		/// 在所有子节点中获得从根节点到目标节点的最小行动力(AP最少)的节点集合
 		/// </summary>
-		public static LinkedList<WarChessCell> GeneratorWay_MinimumAP(WarChessNode<WarChessCell> root, WarChessCell cell) {
-			return GeneratorWay_MinimumAP(root, cell.x, cell.y);
-		}
-
-		private static WarChessNode<WarChessCell> _FindMinLayerNode(WarChessNode<WarChessCell> root, int x, int y) {
-			WarChessNode<WarChessCell> ret = null;
-			foreach (var item in root.children) {
-				if ((item.value.x == x) && (item.value.y == y)) {
-					ret = item;
-					break;
-				}
-			}
-			if (ret == null) {
-				// 如果当前层级下没有找到对应的cell则继续往深层找
-				foreach (var item in root.children) {
-					var temp = _FindMinLayerNode(item, x, y);
-					if (temp == null) { continue; }
-					if (ret == null || ret.layer > temp.layer) {
-						ret = temp;
-					}
-				}
-			}
-			return ret;
+		public static LinkedList<WarChessCell> GeneratorWay(WarChessNode<WarChessCell> root, WarChessCell cell) {
+			return GeneratorWay(root, cell.x, cell.y);
 		}
 
 		private static WarChessNode<WarChessCell> _FindMinAPNode(WarChessNode<WarChessCell> root, int x, int y) {
@@ -113,57 +83,52 @@ namespace WarChess {
 			return ret;
 		}
 
-		private static void _GeneratorCellListFromChildren(WarChessNode<WarChessCell> node, in LinkedList<WarChessCell> li) {
-			if (node.value == null) { return; }
+		private static void _SearchActionSideCells(WarChessNode<WarChessCell> root, WarChessSpace space, WarChessSprite sprite) {
+			var pool = new Queue<WarChessNode<WarChessCell>>();
+			var close = new Dictionary<WarChessCell, WarChessNode<WarChessCell>>();
 
-			foreach (var item in node.children) {
-				if (item.value == null) { continue; }
-				if (li.Find(item.value) == null) {
-					li.AddLast(item.value);
-				}
-			}
-			foreach (var item in node.children) {
-				_GeneratorCellListFromChildren(item, in li);
-			}
-		}
+			pool.Enqueue(root);
+			close.Add(root.value, root);
+			var tempArr = new WarChessCell[4];
+			while (pool.Count > 0) {
+				var node = pool.Dequeue();
 
-		private static void _SearchActionSideCells(WarChessNode<WarChessCell> node, WarChessSpace space, WarChessSprite sprite, float ap, float sumCostAP) {
-			_SearchActionCell(node.value.top, space, node, sprite, ap, sumCostAP);
-			_SearchActionCell(node.value.bottom, space, node, sprite, ap, sumCostAP);
-			_SearchActionCell(node.value.left, space, node, sprite, ap, sumCostAP);
-			_SearchActionCell(node.value.right, space, node, sprite, ap, sumCostAP);
-		}
+				tempArr[0] = node.value.top;
+				tempArr[1] = node.value.bottom;
+				tempArr[2] = node.value.left;
+				tempArr[3] = node.value.right;
 
-		private static void _SearchActionCell(WarChessCell cell, WarChessSpace space, WarChessNode<WarChessCell> parent, WarChessSprite sprite, float ap, float sumCostAP) {
-			if (cell == null) { return; }
-			// 当前Sprite是否可以通行这个地形
-			if (!sprite.TryGetTerrainActionCost(cell.terrain, out var cost)) { return; }
-			// AP是否足够
-			if (ap < cost) { return; }
-			// 这个单元格内的其他Sprite是否为我方或友方阵营
-			var destSpr = space.GetSprite(cell.x, cell.y);
-			if ((destSpr != null) && !sprite.IsFriendlyCamp(destSpr)) { return; }
-
-			var tempParent = parent.parent;
-			float newSumCostAP = sumCostAP + cost;
-			while (tempParent != null) {
-				// 不重复
-				if (tempParent.value == cell) {
-					return;
-				}
-				foreach (var child in tempParent.children) {
-					if (child.value == cell) {
-						// 已有消耗更少的单元格包含了当前格子
-						if (child.customData < newSumCostAP && child.layer <= parent.layer) {
-							return;
+				for (int i = 0; i < tempArr.Length; ++i) {
+					var value = tempArr[i];
+					if (_CheckActionCell(value, space, node, sprite, out var sumCostAP)) {
+						if (close.TryGetValue(value, out var f)) {
+							if (sumCostAP >= f.customData) {
+								continue;
+							} else {
+								close.Remove(value);
+							}
 						}
+						var newNode = node.AddChild(tempArr[i]);
+						newNode.customData = sumCostAP;
+						pool.Enqueue(newNode);
+						close.Add(value, newNode);
 					}
 				}
-				tempParent = tempParent.parent;
 			}
-			var newNode = parent.AddChild(cell);
-			newNode.customData = newSumCostAP;
-			_SearchActionSideCells(newNode, space, sprite, ap - cost, newSumCostAP);
+		}
+
+		private static bool _CheckActionCell(WarChessCell cell, WarChessSpace space, WarChessNode<WarChessCell> parent, WarChessSprite sprite, out float sumCostAP) {
+			sumCostAP = 0;
+			if (cell == null) { return false; }
+			// 当前Sprite是否可以通行这个地形
+			if (!sprite.TryGetTerrainActionCost(cell.terrain, out var cost)) { return false; }
+			// AP是否足够
+			if (parent.customData + cost > sprite.GetBaseActionPoint()) { return false; }
+			// 这个单元格内的其他Sprite是否为我方或友方阵营
+			var destSpr = space.GetSprite(cell.x, cell.y);
+			if ((destSpr != null) && !sprite.IsFriendlyCamp(destSpr)) { return false; }
+			sumCostAP = parent.customData + cost;
+			return true;
 		}
 	}
 }
